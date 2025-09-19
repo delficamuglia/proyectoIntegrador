73f53317b047c515808c5ee48d953af2850e169f
juanabernstein <jbernstein@udesa.edu.ar>
2025-09-18 23:20:48 -0300

    trabajando en resultados de busqueda, ya casi terminado

--- src/Components/Formulario/Formulario.js
+++ src/Components/Formulario/Formulario.js
@@ -1,17 +1,20 @@
 import React, {Component} from "react"
 import { withRouter } from "react-router-dom"
+import './Formulario.css'
+// hacer css del formulario 
 
 class Formulario extends Component {
     constructor(props){
         super(props)
         this.state = {
             busqueda: "",
+            tipo: '',
         }
     }
 
     prevenirRecarga(event) {
         event.preventDefault()
-        this.props.history.push("/busqueda/" + this.state.busqueda)
+        this.props.history.push("/busqueda/" + this.state.tipo + '/'+ this.state.busqueda)
         console.log(event);
         
     }
@@ -25,13 +28,31 @@ class Formulario extends Component {
         
     }
 
+    controlarTipo(event){
+        console.log(event.target.value);
+
+        this.setState({
+            tipo: event.target.value
+        }, ()=> console.log(this.state.tipo))
+        
+    }
+
     render() {        
         return(
             <>
-            <form onSubmit={(event)=>this.prevenirRecarga(event)}>
-                <input type="text" onChange={(event)=> this.controlarCambios(event)} value={this.state.busqueda}/>
+            
+            <form  onSubmit={(event)=>this.prevenirRecarga(event)}>
+                <input  type="text" onChange={(event)=> this.controlarCambios(event)} value={this.state.busqueda}/>
+
+                <h1> Peliculas </h1>
+                <input type="radio" name="tipo" onChange={(event)=> this.controlarTipo(event)} value='movie' />
+
+                <h1> Series </h1>
+                <input type="radio" name="tipo" onChange={(event)=> this.controlarTipo(event)} value='tv' />
+                 
                 <button type="submit"> Buscar</button>
             </form>
+            
             </>
             
         )
--- src/Screens/ResultadosBusqueda/ResultadosBusqueda.js
+++ src/Screens/ResultadosBusqueda/ResultadosBusqueda.js
@@ -1,1 +1,69 @@
- //
+import React, { Component } from "react"
+import PeliculasCards from "../Peliculas/PeliculasCards"
+import SeriesCards from "../Series/SeriesCards"
+// cuando buscas otra cosa hay que recargar todo VER ESO Y PREGUNTAR
+
+class ResultadosBusqueda extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        elementos: [],
+    }
+  }
+
+  componentDidMount() {
+    const query = this.props.match.params.query;
+    const tipo = this.props.match.params.tipo; 
+    const API_KEY = "cc9626b1c01cc6df9ddb2a9c71454130";
+
+    let url = "";
+
+    if (tipo === "movie") {
+        url = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${API_KEY}`;
+    } else if (tipo === "tv") {
+        url = `https://api.themoviedb.org/3/search/tv?query=${query}&api_key=${API_KEY}`;
+    }
+    
+  
+    fetch(url)
+      .then(res => res.json())
+      .then((data)=>{
+        console.log(data)
+        this.setState({ elementos: data.results})
+    
+      })
+      .catch((error)=>{
+        console.log(error)
+     
+      });
+  }
+
+  render(){
+  
+    return (
+      <>
+
+        <h2 className="alert alert-primary">
+            
+     
+        </h2>
+        <section className="row cards all-movies" id="movies">
+            {
+               this.state.elementos.length === 0 ? ( 
+                <h3>No se encontraron resultados de búsqueda</h3> 
+              ) : (
+                this.state.elementos.map((elemento) => ( 
+                 this.props.match.params.tipo === 'movie' ? <PeliculasCards key={elemento.id} pelicula={elemento} /> : <SeriesCards key={elemento.id} serie={elemento} /> 
+                ))
+              )
+            }
+        </section>
+
+       
+      </>
+    )
+  }
+}
+
+export default ResultadosBusqueda
+
