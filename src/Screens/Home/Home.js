57f2bec3ce7f550f420686430335da477164d164
juanabernstein <jbernstein@udesa.edu.ar>
2025-09-19 21:47:08 -0300

    haciendo el punto del home; sus dos grupos de contenidos con los cuatro elemetnos

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,11 +1,74 @@
 import React, {Component} from "react";
-// completar PelisCAartedl y etc y despues importar aca y crear funcion
+import PeliculasCards from "../Peliculas/PeliculasCards";
+import SeriesCards from "../Series/SeriesCards";
+import { Link } from "react-router-dom";
+
 
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            peliculasDescubrir: [],
+            //
+            seriesDescubrir: [],
+            //
+    
+          
+        }
+      }    
+
+    componentDidMount() {
+    
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=cc9626b1c01cc6df9ddb2a9c71454130&language=es-ES&page=1")
+          .then(response => response.json())
+          .then(data => this.setState({
+            peliculasDescubrir: data.results,
+            // 
+          }))
+          .catch(error => console.log(error));
+
+          fetch("https://api.themoviedb.org/3/discover/tv?api_key=cc9626b1c01cc6df9ddb2a9c71454130&language=es-ES&page=1")
+          .then(response => response.json())
+          .then(data => this.setState({
+            seriesDescubrir: data.results,
+            //
+          }))
+          .catch(error => console.log(error));
+      }
+
+      
+
     render () {
         return (
-            <h2>Hola</h2>
+         
+                <>
+                 <h2 className="alert alert-primary">Descubrir películas</h2>
+                 <Link to='/peliculas'> Ver todas </Link>
+                 <section className="row cards all-movies" id="movies">
+            {
+               (
+                this.state.peliculasDescubrir.map((pelicula, idx) => ( //si que el length de los resultados es 0 es falso, y que la app esta cargando es falso entonces mapeamos los resultados y los mostramos 
+                 idx<4 ? <PeliculasCards key={pelicula.id} pelicula={pelicula} /> : null
+                ))
+              )
+            }
+        </section>
+
+        <h2 className="alert alert-primary">Descubrir  series</h2>
+        <Link to='/series'> Ver todas </Link>
+                 <section className="row cards all-movies" id="movies">
+            {
+               (
+                this.state.seriesDescubrir.map((serie, idx) => ( //si que el length de los resultados es 0 es falso, y que la app esta cargando es falso entonces mapeamos los resultados y los mostramos 
+                 idx<4 ? <SeriesCards key={serie.id} serie={serie} /> : null 
+                ))
+              )
+            }
+        </section>
+        </>
+            
         )
     }
 }
